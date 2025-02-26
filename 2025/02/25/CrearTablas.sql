2f635ff6bf8388af8448d3208db1634deb482fff
Eduar Yair Aguirre Ceballos <L23170037@culiacan.tecnm.mx>
2025-02-26 18:00:51 +0000

    procedimiento

--- 2025/02/25/CrearTablas.sql
+++ 2025/02/25/CrearTablas.sql
@@ -1,29 +1,54 @@
-    create database escelita;
+    create database escuelita;
 
-    go;
+    GO 
 
-    use escuelita
+    USE escuelita
     
-    Create TABLE Persona(
+    CREATE TABLE Persona(
              CURP CHAR(18) NOT NULL PRIMARY KEY,
              Nombre NVARCHAR(50) NOT NULL,
              Direccion NVARCHAR(100) NULL,
              Nacimiento DATE NOT NULL,
     )
-    create TABLE Carrera(
+    CREATE TABLE Carrera(
 
-        Clase CHAR(13) NOT NULL PRIMARY KEY,
+        Clave CHAR(13) NOT NULL PRIMARY KEY,
         Nombre VARCHAR(60) NOT NULL,
 
     )
-    Create TABLE Alumno(
+    CREATE TABLE Alumno(
         CURP CHAR(18) NOT NULL PRIMARY KEY FOREIGN KEY REFERENCES Persona(CURP),
         NumeroDeControl CHAR(8) NOT NULL UNIQUE,
-        Carrera CHAR(13) FOREIGN KEY REFERENCES Carrera(Clase),
+        ClaveCarrera CHAR(13) FOREIGN KEY REFERENCES Carrera(Clase),
 
     )
-    select * from persona;
+
+    CREATE TABLE Docente(
+        CURP CHAR(18) NOT NULL PRIMARY KEY,
+        RFC CHAR(13) NOT NULL UNIQUE,
+        Profesion NVARCHAR (30) NULL,
+        FOREIGN KEY (CURP) REFERENCES Persona(CURP)
+
+    )
+
+    CREATE TABLE Administrativo(
+        CURP CHAR (18) NOT NULL
+        PRIMARY KEY
+        FOREIGN KEY REFERENCES Persona(CURP)
+    )
+    
+    INSERT INTO carrera(clase,Nombre)
+    VALUES('ITIC-2010-225', 'sistemas computacionales'),
+            ('IMCT-2010-229', 'Ingeniera Mecatronica')
+
+    INSERT INTO Persona(CURP, Nombre, Direccion, Nacimiento)
+        VALUES('J520FR', 'Jphane Sacrebleu', 'por alla', '2003-04-05')
 
 
+    INSERT INTO Alumno (CURP, NumeroDeControl, ClaveCarrera)
+        VALUES('J520FR', '20170005', 'ITIC-2010-225')
+
+    SELECT NumeroDeControl, Nombre, ClaveCarrera FROM Persona
+        join Alumno on Persona.CURP = Alumno.CURP
     
 
