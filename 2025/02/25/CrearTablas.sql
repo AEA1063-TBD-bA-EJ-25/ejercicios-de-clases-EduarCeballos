dc19ba5b6b77e55d35f16e3f836df378738ed73e
Eduar Yair Aguirre Ceballos <L23170037@culiacan.tecnm.mx>
2025-03-04 17:57:16 +0000

    constraint

--- 2025/02/25/CrearTablas.sql
+++ 2025/02/25/CrearTablas.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE escuelita;
+CREATE DATABASE escuelita 
 
 go
 
@@ -72,3 +72,39 @@ INSERT into Alumno(CURP, NumeroDeControl, ClaveCarrera)
     INSERT into MalaPersona(CURP, Nombre, Direccion, Nacimiento)
     VALUES ('J520FR', 'johane sacrebleu','por alla', '2003-04-05')
 
+CREATE TABLE empleado(
+    idEmpleado int not null PRIMARY KEY,
+    nombre NVARCHAR(50) not null,
+    sueldo money DEFAULT 1500
+)
+INSERT INTO empleado(idEmpleado, nombre, sueldo)
+VALUES(1, 'Felipe de jesus', 2000)
+
+INSERT INTO empleado(idEmpleado, nombre)
+VALUES (2, 'chuerk' )  
+
+SELECT * FROM empleado
+
+CREATE TABLE personita(
+    idEmpleado int not null PRIMARY key,
+    nombre NVARCHAR(50) not null,
+    sueldo money DEFAULT 1500,
+    telefono VARCHAR(20) check (telefono like '667(0-9) (0-9) (0-9)')
+)
+INSERT into personita VALUES(1, 'juana', null, '667123')
+
+ALTER TABLE empleado
+    add sexo CHAR(1) null check (sexo in('H', 'M'))
+
+SELECT * from empleado
+insert into empleado(idEmpleado, nombre, sexo) values (3, 'Alexis Jara Rodriguez', 'H')
+insert into empleado(idEmpleado, nombre, sexo) values (4, 'Matilde Sandoval', 'M')
+
+alter table empleado
+drop CONSTRAINT CK_empleado_sexo_3F466844
+
+insert into empleado (idEmpleado, nombre, sexo) values(4, 'Matilde Sandoval', 'H')
+SELECT * FROM empleado
+
+ALTER table empleado
+    ADD CONSTRAINT sexonuevaopciones
